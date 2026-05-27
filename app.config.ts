0948b0876fa1fea9087b545c4bad4e20d3bc25c7
Temur662 <temurbeksayfutdinov@gmail.com>
2026-05-27 12:51:21 -0400

    chore: updated version

--- app.config.ts
+++ app.config.ts
@@ -37,7 +37,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: masjid.displayName,
   slug: "sahla",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/images/sahla-logo-arabic.png",
   scheme: `sahla-${MASJID_ID}`,
